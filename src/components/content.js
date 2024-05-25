786f79e5d184c9384876f292d856f5bcf7dc71eb
adarsh <adarshsai619@gmail.com>
2024-05-25 14:41:09 +0530

    initial commit

--- src/components/content.js
+++ src/components/content.js
@@ -1,17 +1,19 @@
 // Content.js
 import React from 'react';
 
+
+
 const Content = () => {
     return (
         <div>
             <h2>Content Page</h2>
             {/* Add your course names, descriptors, and links here */}
-            <p>Course 1</p>
-            <p>Description: This is course 1</p>
+            <p>NEET</p>
+            <p>Description: Crash Course for need </p>
             <a href="https://www.youtube.com/watch?v=zY38SV2BW7k&list=PLJyab0VQDBGWVYHPLjAHjw9y1YB9Gf4bf">Link to Course 1</a>
             
-            <p>Course 2</p>
-            <p>Description: This is course 2</p>
+            <p>Mathematics</p>
+            <p>Description: Math Series</p>
             <a href="https://www.youtube.com/watch?v=cOVMwNdrnuM&list=PLF_7kfnwLFCE9j-sCe6f1zvG8QDbGgQUT&pp=iAQB">Link to Course 2</a>
             
             {/* Add more courses as needed */}
